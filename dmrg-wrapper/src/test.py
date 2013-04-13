e62329f16d1f2938cd2cc58bbe58fa1a5d62e4ec
Mark <murphy.mark.w@gmail.com>
2013-04-13 17:37:04 -0400

    Testing freeing a previously allocated memory array that was passed to Python via ctypes. Note that there is a segfault due to freeing of an unallocated space. Cause?

--- dmrg-wrapper/src/test.py
+++ dmrg-wrapper/src/test.py
@@ -4,6 +4,7 @@ import ctypes
 import numpy
 
 lib = ctypes.cdll.LoadLibrary('dmtk.so')
-lib.get_array.restype = numpy.ctypeslib.ndpointer(dtype=numpy.float64,ndim=1,shape=(5,))
+#lib.get_array.restype = numpy.ctypeslib.ndpointer(dtype=numpy.float64,ndim=1,shape=(5,))
 a = lib.get_array(5)
-for i in a: print i
+#for i in a: print i
+lib.free_array(a)
--- dmrg-wrapper/src/test_functions.cpp
+++ dmrg-wrapper/src/test_functions.cpp
@@ -1,8 +1,11 @@
 #ifndef __TEST_FUNCTIONS_CPP__
 #define __TEST_FUNCTIONS_CPP__
 
+#include <cstdlib>
+
 extern "C" {
   double * get_array(int s) { return new double[s]; }
+  void free_array(void* a) { free(a); }
 };
 
 #endif//__TEST_FUNCTIONS_CPP__
