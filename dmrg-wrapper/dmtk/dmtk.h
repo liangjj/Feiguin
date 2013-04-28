eb27b3fd9e08d5b837505e68c4b121545a52aa87
Mark <murphy.mark.w@gmail.com>
2013-04-28 13:47:16 -0400

    Adding alternate dmtk library.

---
+++ dmrg-wrapper/dmtk/dmtk.h
@@ -0,0 +1,23 @@
+#ifndef __DMTK_H__
+#define __DMTK_H__
+
+namespace dmtk
+{
+
+enum enums_dmtk
+{
+  LEFT,
+  RIGHT,
+  CENTER,
+};
+
+} //namespace dmtk
+
+#include <math.h>
+#include <valarray>
+#include <dmtk/enums.h>
+#include <dmtk/vector.h>
+#include <dmtk/matrix.h>
+#include <dmtk/lanczos.h>
+
+#endif // __DMTK_H__
